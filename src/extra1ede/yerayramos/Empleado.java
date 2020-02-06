73764758525f16d14eef040838c9c9c550f5a904
lleri <43353563+lleri97@users.noreply.github.com>
2020-02-06 16:06:59 +0100

    Add files via upload

---
+++ src/extra1ede/yerayramos/Empleado.java
@@ -0,0 +1,27 @@
+package extra1ede.yerayramos;
+
+public class Empleado {
+
+    private String nombre;
+
+    private String apellido;
+
+    private double salario;
+
+    public Empleado(String nombre, String apellido, double salario) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.salario = salario;
+    }
+
+    public void subirsalario(double subida) {
+        salario = salario + subida;
+    }
+
+    private boolean comprobar() {
+        if (nombre.equals("")) {
+            return false;
+        }
+        return true;
+    }
+}
